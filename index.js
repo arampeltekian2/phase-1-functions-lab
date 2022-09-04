36ffd01668d628e9073e72a2e4c5d0057590d232
Aram <arampeltekian2@gmail.com>
2022-09-04 10:44:41 -0500

    commit tree 5e65dda5d064279d710e0227d03778403375ab41

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+ if (blocks > 42){
+ return blocks - 42;
+ } else {
+  return 42- blocks 
+ }
+}
+
+function distanceFromHqInFeet(feet){
+  if (feet > 2112){
+    return feet - 2112; 
+  }else {
+    return 2112 - feet
+  }
+  }
+
+  
